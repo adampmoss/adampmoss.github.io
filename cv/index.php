bf75d730ed045751cef1fc54b2e2eefef702d1ff
Adam Moss <adammoss7@gmail.com>
2017-03-24 18:22:49 +0000

    adds CV

---
+++ cv/index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="robots" content="noindex nofollow" />
+        <title>Adam Moss - CV</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,700,900,500,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+		<link rel="stylesheet" href="./css/animate.css">
+		<link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+
+    <?php 
+    	$twitter = "http://twitter.com/adampmoss";
+    	$linkedin = "https://uk.linkedin.com/pub/adam-moss/31/734/11b";
+    	$email = "adammoss7@gmail.com";
+    	$github = "https://github.com/adampmoss";
+    ?>
+	
+    <div class="container">
+    	<div class="row">
+    		<div class="col-xs-12 col-md-9  animated slideInLeft">
+	        	<h1 class="cover-heading">Adam Moss</h1>
+	        	<p class="lead">I'm a <a href="http://tinyurl.com/na9kbma" target="_blank">3x Magento Certified</a> web developer with many years of experience who has a variety of skills ranging from team coaching, implementation of standards and processes, client communication, scoping and quoting, R&amp;D, documentation and problem solving. I'm hardworking and have very good commercial awareness from being involved business operations and decision making.</p>
+	        	<ul class="list-inline">
+  					<li><a href="<?php echo $twitter ?>" class="btn btn-default btn-sm" role="button" target="_blank"><i class="fa fa-twitter"></i> Twitter</a>
+  					<span class="print-link"><i class="fa fa-twitter"></i> <?php echo $twitter ?></span></li>
+  					<li><a href="<?php echo $linkedin ?>" class="btn btn-default btn-sm" role="button" target="_blank"><i class="fa fa-linkedin"></i> LinkedIn</a>
+  					<span class="print-link"><i class="fa fa-linkedin"></i> <?php echo $linkedin ?></span></li>
+  					<li><a href="mailto:<?php echo $email ?>" class="btn btn-default btn-sm" role="button"><i class="fa fa-envelope"></i> Email me</a><span class="print-link"><i class="fa fa-envelope"></i> <?php echo $email ?></span></li>
+  					<li><a href="mailto:<?php echo $github ?>" class="btn btn-default btn-sm" role="button"><i class="fa fa-github"></i> GitHub</a><span class="print-link"><i class="fa fa-github"></i> <?php echo $github ?></span></li>
+				</ul>
+	        </div>
+	        <div class="col-sm-3 hidden-xs hidden-sm animated slideInRight">
+	        	<img src="./img/me.jpg" class="img-responsive me" />
+	        </div>
+	    </div>
+	    <div class="row">
+  			<div class="col-xs-12 col-sm-6 animated slideInLeft left-column">
+  			<h3><i class="fa fa-graduation-cap"></i> Qualifications</h3>
+  			<img src="./img/certification.png" class="img-responsive cert">
+  			<ul>
+  				<li>First Class Degree BA Multimedia Computing</li>
+  				<li>A Level Business, IT, Geography</li>
+  				<li>AS Level Mathematics</li>
+			</ul>
+  			<h3><i class="fa fa-github-alt"></i> Technical Skills</h3>
+  			<ul>
+  				<li>PHP</li>
+  				<li>MySQL</li>
+  				<li>JavaScript / Backbone / JQuery / Prototype</li>
+  				<li>AJAX / XML / JSON / API</li>
+  				<li>Git</li>
+  				<li>Composer / NPM</li>
+  				<li>MAMP / Vagrant</li>
+  				<li>HTML5</li>
+  				<li>SCSS / Sass / Gulp / Bootstrap</li>
+  				<li>Magento</li>
+  				<li>Wordpress</li>
+  				<li>CodeIgniter</li>
+			</ul>
+  			<h3><i class="fa fa-futbol-o"></i> Personal Interests</h3>
+  			<p>I enjoy watching and playing football, and I am commited to exercise which includes the gym, running and cycling. I am also a big fan of outlaw country music and enjoy travelling around Europe and the US.</p>
+  			</div>
+  			<div class="col-xs-12 col-sm-6 animated slideInRight right-column">
+	  			<h3><i class="fa fa-clock-o"></i> Recent Employment History</h3>
+	  			<p><strong>Creare, September 2007-Present</strong></p>
+
+				<p>Web Technical Manager (2014-Present)</p>
+				<ul>
+					<li>Manager of web R&amp;D team</li>
+					<li>Manager of technical quotes team</li>
+					<li>Developed web operations and processes</li>
+					<li>Writing technical quotes / scoping / client consultation</li>
+					<li>Developed Magento &amp; Magento 2 extensions</li>
+					<li>Sales / web team support &amp; coaching</li>
+					<li>Backend web development</li>
+					<li>Led roll out version control to web department</li>
+					<li>Led roll out Cloudflare to business</li>
+					<li>Developed quotes system in CRM</li>
+					<li>Led many colleagues to become Magento certified</li>
+					<li>Creator of Magento Fox</li>
+					<li>Magento blogger</li>
+					<li>Created Magento training videos archive</li>
+				</ul>
+
+				<p>E-commerce Manager (2012-2014)</p>
+				<ul>
+					<li>Responsible for e-commerce product offering, standards, processes, training</li>
+					<li>Developed Creare's e-commerce website</li>
+					<li>Developed bespoke Magento extensions</li>
+					<li>Frontend/backend web development</li>
+				</ul>
+
+				<p>Web Team Leader (2008-2012)</p>
+				<ul>
+					<li>Leader of a team of web designers and developers</li>
+					<li>Client management</li>
+					<li>Project management / scheduling</li>
+					<li>Frontend web development</li>
+				</ul>
+
+				<p>Web Developer (2007-2008)</p>
+				<ul>
+					<li>Web design &amp; frontend web development</li>
+					<li>SEO</li>
+				</ul>
+			</div>
+		</div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	<script>
+		jQuery(document).ready(function($) {
+    		$("body").fadeIn(1500);
+		});
+	</script>
+  </body>
+</html>
